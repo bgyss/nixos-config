86c8fb51d435e138f8bca398f17afa625cc123a3
Brian Gyss <bgyss@hey.com>
2025-09-24 16:38:10 -0700

    bumped yt-dlp to 2025.09.23

--- overlays/20-yt-dlp.nix
+++ overlays/20-yt-dlp.nix
@@ -1,11 +1,11 @@
 final: prev: {
   yt-dlp = prev.yt-dlp.overridePythonAttrs (old: {
-    version = "2025.07.21";
+    version = "2025.09.23";
     src = prev.fetchFromGitHub {
       owner = "yt-dlp";
       repo = "yt-dlp";
-      rev = "2025.08.20";
-      hash = "sha256-FeIoV7Ya+tGCMvUUXmPrs4MN52zwqrcpzJ6Arh4V450=";
+      rev = "2025.09.23";
+      hash = "sha256-pqdR1JfiqvBs5vSKF7bBBKqq0DRAi3kXCN1zDvaW3nQ=";
     };
   });
 } 
