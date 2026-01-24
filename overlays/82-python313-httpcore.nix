bb91d305ab49c3104ebdecbe94acd131263216b0
Brian Gyss <bgyss@hey.com>
2026-01-24 13:16:36 -0800

    overlays: fix python313-httpcore overlay to use overrideScope instead of override
    - Change from `override { overrides = ... }` to `overrideScope` for Python package set
    - Simplifies overlay syntax while maintaining the same functionality of disabling httpcore tests

--- overlays/82-python313-httpcore.nix
+++ overlays/82-python313-httpcore.nix
@@ -1,10 +1,8 @@
 # Disable httpcore tests on Python 3.13 - upstream test failures with async cancellation
 final: prev: {
-  python313Packages = prev.python313Packages.override {
-    overrides = pfinal: pprev: {
-      httpcore = pprev.httpcore.overrideAttrs (old: {
-        doCheck = false;
-      });
-    };
-  };
+  python313Packages = prev.python313Packages.overrideScope (pfinal: pprev: {
+    httpcore = pprev.httpcore.overrideAttrs (old: {
+      doCheck = false;
+    });
+  });
 }
