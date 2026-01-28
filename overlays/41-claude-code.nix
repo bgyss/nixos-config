428e1b3bbe1e96a3a10519e51b104c920a2cca37
Brian Gyss <bgyss@hey.com>
2026-01-27 17:01:59 -0800

    fixed hash

--- overlays/41-claude-code.nix
+++ overlays/41-claude-code.nix
@@ -5,7 +5,7 @@ final: prev: {
 
     src = prev.fetchurl {
       url = "https://storage.googleapis.com/claude-code-dist-86c565f3-f756-42ad-8dfa-d59b1c096819/claude-code-releases/${version}/darwin-arm64/claude";
-      hash = "sha256-xXA1lu2FSujld1zzjeXXHYpBIEocOnjQXjIw=";
+      hash = "sha256-xXA1lu2FSujld1zzjeXXHYpW7P4/NpBIEocOnjQXjIw=";
     };
 
     dontUnpack = true;
