5aafc2e96b671e10b536812fb5900c2157bc5397
Brian Gyss <bgyss@hey.com>
2026-02-26 22:26:31 -0800

    flake.lock: roll back nixpkgs to ea7f1f0 (xar-minimal patch OOM in stdenv), remove libxml2 overlay

--- overlays/84-libxml2.nix
+++
@@ -1,7 +0,0 @@
-final: prev: {
-  libxml2 = prev.libxml2.overrideAttrs (old: {
-    patches = builtins.filter
-      (p: !(builtins.match ".*CVE-2026-0989.*" (toString p) != null))
-      (old.patches or []);
-  });
-}
